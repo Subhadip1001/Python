a641d4462aa10d0376f389f023ef4d7ca3eaafac
Subhadip Mandal <subhadipmandal714@gmail.com>
2023-08-28 11:45:26 +0530

    how to take a input from user

---
+++ take_input_fro_user.py
@@ -0,0 +1,6 @@
+name=input("Enter your name :") #input like a string
+age=int(input("What is your age :")) #input intiger number
+print("Hey" + " " + name)
+print(f"{name} you are {age} year old")
+
+print(name + " Thank you for comunication")
